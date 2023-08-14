2735ac382e27ecb746c766152c2aeda018e7ac89
ohbumpy <kalukelechi254@gmail.com>
2023-08-14 07:29:55 -0700

    brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+
+	for (j = '0' ; j <= '9' ; j++)
+	{
+		for (i = '0' ; i <= '9' ; i++)
+		{
+			if (!((i == j) || (j > i)))
+			{
+				putchar(i);
+				putchar(j);
+				if (!(i == '9' && j == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
